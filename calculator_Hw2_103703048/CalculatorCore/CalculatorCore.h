ed0301a7ae7018a11eb7f6849a39f3f553760d55
Ed <103703048@nccu.edu.tw>
2017-04-19 11:09:31 +0800

    New version

---
+++ calculator_Hw2_103703048/CalculatorCore/CalculatorCore.h
@@ -0,0 +1,19 @@
+//
+//  CalculatorCore.h
+//  CalculatorCore
+//
+//  Created by Ed on 2017/4/18.
+//  Copyright © 2017年 Ed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalculatorCore.
+FOUNDATION_EXPORT double CalculatorCoreVersionNumber;
+
+//! Project version string for CalculatorCore.
+FOUNDATION_EXPORT const unsigned char CalculatorCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorCore/PublicHeader.h>
+
+
